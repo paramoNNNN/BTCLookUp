57b8cf8505201ee655ac4245440301ea4902263f
Taha <taha.ojari@gmail.com>
2022-02-19 20:08:00 +0330

    fix: cleanup index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>BTC Look Up</title>
+				<meta name="description" content="BTC Look Up by hash" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
