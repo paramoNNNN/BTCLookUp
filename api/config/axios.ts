1d2ab46c7530732f8e0ad8370272c0fab368dddc
Taha <taha.ojari@gmail.com>
2022-02-20 22:46:16 +0330

    feat: create axios-client-instance

--- api/config/axios.ts
+++ api/config/axios.ts
@@ -3,7 +3,11 @@ import axios from 'axios';
 export const baseURL = process.env.BLOCKCYPHER_API_URL;
 
 export const axiosInstance = axios.create({
-	baseURL: baseURL,
-	method: 'POST',
-	timeout: 30000,
+  baseURL,
+  timeout: 30000,
+});
+
+export const axiosClientInstance = axios.create({
+  baseURL: '/api',
+  timeout: 30000,
 });
