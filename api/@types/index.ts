262f0f6f03a3bcf0a5ce9709d9dc64e9395177ba
Taha <taha.ojari@gmail.com>
2022-02-20 21:55:41 +0330

    feat: create transaction-response type

--- api/@types/index.ts
+++ api/@types/index.ts
@@ -24,3 +24,41 @@ export type AddressResponse = {
 	txrefs: AddressTRXRef[];
 	tx_url: string;
 };
+
+export type TransactionResponse = {
+	block_hash: string;
+	block_height: number;
+	block_index: number;
+	hash: string;
+	addresses: string[];
+	total: number;
+	fees: number;
+	size: number;
+	vsize: number;
+	preference: string;
+	confirmed: string;
+	received: string;
+	ver: number;
+	double_spend: boolean;
+	vin_sz: number;
+	vout_sz: number;
+	confirmations: number;
+	confidence: number;
+	inputs: {
+		prev_hash: string;
+		output_index: number;
+		script: string;
+		output_value: number;
+		sequence: number;
+		addresses: string[];
+		script_type: string;
+		age: number;
+	}[];
+	outputs: {
+		value: number;
+		script: string;
+		spent_by: string;
+		addresses: string[];
+		script_type: string;
+	}[];
+};
