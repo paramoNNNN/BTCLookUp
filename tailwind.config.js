29c4f063abc4d8fce5ead8d5697373cdc7e0ef72
Taha <taha.ojari@gmail.com>
2022-02-20 22:46:02 +0330

    chore(tailwind): add features to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./layout/**/*.{js,ts,jsx,tsx}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
