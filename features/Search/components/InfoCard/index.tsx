7bf84c38ce57737c5ce2892ad41d1d19dfc3ec22
Taha <taha.ojari@gmail.com>
2022-02-22 21:37:08 +0330

    feat: add transaction fields to info-card

--- features/Search/components/InfoCard/index.tsx
+++ features/Search/components/InfoCard/index.tsx
@@ -2,26 +2,41 @@ import { useMemo } from 'react';
 import type { SearchTypes } from 'features/Search/@types';
 import type { AddressResponse, TransactionResponse } from 'api/@types';
 import {
+  BadgeCheckIcon,
   CashIcon,
   CheckCircleIcon,
   ClockIcon,
+  CurrencyDollarIcon,
+  DatabaseIcon,
   DocumentDownloadIcon,
+  DocumentIcon,
+  LoginIcon,
+  LogoutIcon,
   MinusCircleIcon,
   PlusCircleIcon,
   XCircleIcon,
 } from '@heroicons/react/solid';
 
 type Props = {
-  title: string;
   data: TransactionResponse | AddressResponse;
   type: SearchTypes;
 };
 
-const InfoCard = ({ title, data, type }: Props) => {
+const typeLabels: Record<SearchTypes, string> = {
+  transaction: 'Transaction',
+  address: 'Address',
+};
+
+const InfoCard = ({ data, type }: Props) => {
   const fields = useMemo(() => {
     if (type === 'transaction') {
       const transaction = data as TransactionResponse;
       return [
+        {
+          icon: <DocumentIcon className="w-6 h-6 text-gray-500" />,
+          title: 'Hash',
+          value: transaction.hash,
+        },
         {
           icon: <ClockIcon className="w-6 h-6 text-slate-500" />,
           title: 'Received time',
@@ -36,6 +51,31 @@ const InfoCard = ({ title, data, type }: Props) => {
             ),
           title: 'Status',
           value: transaction.confirmations > 0 ? 'Confirmed' : 'Not confirmed',
+        },
+        {
+          icon: <DatabaseIcon className="w-6 h-6 text-orange-500" />,
+          title: 'Size',
+          value: transaction.size,
+        },
+        {
+          icon: <BadgeCheckIcon className="w-6 h-6 text-green-500" />,
+          title: 'Confirmations',
+          value: transaction.confirmations,
+        },
+        {
+          icon: <LoginIcon className="w-6 h-6 text-sky-500" />,
+          title: 'Total inputs',
+          value: transaction.inputs.length,
+        },
+        {
+          icon: <LogoutIcon className="w-6 h-6 text-rose-500" />,
+          title: 'Total outputs',
+          value: transaction.outputs.length,
+        },
+        {
+          icon: <CurrencyDollarIcon className="w-6 h-6 text-yellow-500" />,
+          title: 'Total fees',
+          value: transaction.fees,
         },
       ];
     }
@@ -80,8 +120,10 @@ const InfoCard = ({ title, data, type }: Props) => {
   }, [data, type]);
 
   return (
-    <div className="bg-white border border-slate-300 py-4 px-6 space-y-6 rounded-md">
-      <h5 className="text-xl font-semibold text-slate-900">{title}</h5>
+    <div className="bg-white border border-slate-300 py-4 px-6 space-y-4 rounded-md">
+      <h5 className="text-xl font-semibold text-slate-900 break-all">
+        {typeLabels[type]}
+      </h5>
       <ul className="flex flex-col gap-3">
         {fields.map(({ icon, title, value }) => (
           <li key={title} className="flex items-center space-x-2">
