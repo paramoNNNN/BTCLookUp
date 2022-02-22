01f10f00fd612180c4d1d062e17ce40ebcfc546e
Taha <taha.ojari@gmail.com>
2022-02-22 22:16:18 +0330

    feat: add query-client-provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<title>BTC Look Up</title>
-				<meta name="description" content="BTC Look Up by hash" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  const queryClient = new QueryClient();
 
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>BTC Look Up</title>
+        <meta name="description" content="BTC Look Up by hash" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default MyApp;
