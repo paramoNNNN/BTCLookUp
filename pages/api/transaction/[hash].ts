9a05cdd08729f830469d72ad2d14b7f5fb243284
Taha <taha.ojari@gmail.com>
2022-02-23 22:00:01 +0330

    feat(api): return subscribed status in transaction and address routes

--- pages/api/transaction/[hash].ts
+++ pages/api/transaction/[hash].ts
@@ -1,30 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { TransactionResponse } from 'api/@types';
 import { axiosInstance } from 'api/config/axios';
 import { endpoints } from 'api/config/endpoints';
 import { checkRequest } from 'api/utils';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from 'api/utils/supabase';
+import { SUBSCRIBED_HASHES_TABLE } from 'api/consts';
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<TransactionResponse>
+  req: NextApiRequest,
+  res: NextApiResponse<TransactionResponse>
 ) {
-	const { hash } = req.query;
-	if (
-		!checkRequest({
-			req,
-			res,
-			acceptableMethods: ['GET'],
-		})
-	) {
-		return;
-	}
+  const { hash } = req.query;
+  const { user } = req.body;
+  if (
+    !checkRequest({
+      req,
+      res,
+      acceptableMethods: ['POST'],
+    })
+  ) {
+    return;
+  }
 
-	const response = await axiosInstance
-		.get<TransactionResponse>(`${endpoints.txs}/${hash}`)
-		.catch(({ response }) => {
-			res.status(response.status).json(response.data);
-		});
-	if (response) {
-		res.status(200).json({ ...response.data });
-	}
+  const { data: subscribedHash } = await supabase
+    .from(SUBSCRIBED_HASHES_TABLE)
+    .select()
+    .match({ hash, user })
+    .single();
+
+  const response = await axiosInstance
+    .get<TransactionResponse>(`${endpoints.txs}/${hash}`)
+    .catch(({ response }) => {
+      res.status(response.status).json(response.data);
+    });
+  if (response) {
+    res.status(200).json({ ...response.data, subscribed: !!subscribedHash });
+  }
 }
