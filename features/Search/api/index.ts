af133f572e5192c614ca051392bcd5b32293e4f8
Taha <taha.ojari@gmail.com>
2022-02-23 21:05:40 +0330

    feat: create subscribe-to-hash in search api

--- features/Search/api/index.ts
+++ features/Search/api/index.ts
@@ -1,6 +1,12 @@
-import type { AddressResponse, TransactionResponse } from 'api/@types';
+import type {
+  AddressResponse,
+  SubscribeResponse,
+  TransactionResponse,
+} from 'api/@types';
 import { axiosClientInstance } from 'api/config/axios';
 import { clientEndpoints } from 'api/config/endpoints';
+import { getUser } from 'utils';
+import { SearchTypes } from '../@types';
 
 type GetTransactionParams = {
   hash: string;
@@ -20,6 +26,22 @@ type GetAddressParams = {
 export const getAddress = async ({ address }: GetAddressParams) => {
   const { data } = await axiosClientInstance.get<AddressResponse>(
     `${clientEndpoints.address}/${address}`
+  );
+  return data;
+};
+
+type SubscribeToHashParams = {
+  hash: string;
+  type: SearchTypes;
+};
+
+export const subscribeToHash = async ({
+  hash,
+  type,
+}: SubscribeToHashParams) => {
+  const { data } = await axiosClientInstance.post<SubscribeResponse>(
+    `${clientEndpoints.subscribe}/${hash}`,
+    { user: getUser(), type }
   );
   return data;
 };
