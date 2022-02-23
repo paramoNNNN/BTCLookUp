6532b4462b4e585f92febc1aad8fb2f2949168b1
Taha <taha.ojari@gmail.com>
2022-02-23 21:04:48 +0330

    feat: create subscribe-response type

--- api/@types/index.ts
+++ api/@types/index.ts
@@ -1,64 +1,78 @@
+type BaseResponse = {
+  subscribed: boolean;
+};
+
 export type AddressTRXRef = {
-	trx_hash: string;
-	block_height: number;
-	tx_input_n: number;
-	tx_output_n: number;
-	value: number;
-	ref_balance: number;
-	spent: boolean;
-	confirmations: number;
-	confirmed: string;
-	double_spend: boolean;
+  trx_hash: string;
+  block_height: number;
+  tx_input_n: number;
+  tx_output_n: number;
+  value: number;
+  ref_balance: number;
+  spent: boolean;
+  confirmations: number;
+  confirmed: string;
+  double_spend: boolean;
 };
 
-export type AddressResponse = {
-	address: string;
-	total_received: number;
-	total_sent: number;
-	balance: number;
-	unconfirmed_balance: number;
-	final_balance: number;
-	n_tx: number;
-	unconfirmed_n_tx: number;
-	final_n_tx: number;
-	txrefs: AddressTRXRef[];
-	tx_url: string;
-};
+export interface AddressResponse extends BaseResponse {
+  address: string;
+  total_received: number;
+  total_sent: number;
+  balance: number;
+  unconfirmed_balance: number;
+  final_balance: number;
+  n_tx: number;
+  unconfirmed_n_tx: number;
+  final_n_tx: number;
+  txrefs: AddressTRXRef[];
+  tx_url: string;
+}
 
-export type TransactionResponse = {
-	block_hash: string;
-	block_height: number;
-	block_index: number;
-	hash: string;
-	addresses: string[];
-	total: number;
-	fees: number;
-	size: number;
-	vsize: number;
-	preference: string;
-	confirmed: string;
-	received: string;
-	ver: number;
-	double_spend: boolean;
-	vin_sz: number;
-	vout_sz: number;
-	confirmations: number;
-	confidence: number;
-	inputs: {
-		prev_hash: string;
-		output_index: number;
-		script: string;
-		output_value: number;
-		sequence: number;
-		addresses: string[];
-		script_type: string;
-		age: number;
-	}[];
-	outputs: {
-		value: number;
-		script: string;
-		spent_by: string;
-		addresses: string[];
-		script_type: string;
-	}[];
+export interface TransactionResponse extends BaseResponse {
+  block_hash: string;
+  block_height: number;
+  block_index: number;
+  hash: string;
+  addresses: string[];
+  total: number;
+  fees: number;
+  size: number;
+  vsize: number;
+  preference: string;
+  confirmed: string;
+  received: string;
+  ver: number;
+  double_spend: boolean;
+  vin_sz: number;
+  vout_sz: number;
+  confirmations: number;
+  confidence: number;
+  inputs: {
+    prev_hash: string;
+    output_index: number;
+    script: string;
+    output_value: number;
+    sequence: number;
+    addresses: string[];
+    script_type: string;
+    age: number;
+  }[];
+  outputs: {
+    value: number;
+    script: string;
+    spent_by: string;
+    addresses: string[];
+    script_type: string;
+  }[];
+}
+
+export type SubscribeResponse = {
+  data?: {
+    id: number;
+    hash: string;
+    user: string;
+    created_at: string;
+  }[];
+  status: 'created' | 'deleted';
 };
