67b1da1b86b3f3fc703d0d415fce9f206a74c842
Taha <taha.ojari@gmail.com>
2022-02-24 23:39:40 +0330

    feat: create address and transaction routes

---
+++ pages/address/[query].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Search from 'features/Search';
+
+const AddressPage: NextPage = () => {
+  return <Search />;
+};
+
+export default AddressPage;
---
+++ pages/transaction/[query].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Search from 'features/Search';
+
+const SearchPage: NextPage = () => {
+  return <Search />;
+};
+
+export default SearchPage;
