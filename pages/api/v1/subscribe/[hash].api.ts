fb0c13b6b2546c59a5edbab2d4ac2a97f3ef8873
Taha <taha.ojari@gmail.com>
2022-02-25 10:46:06 +0330

    fix(api): fix deleting subscribed hash with notifications

--- pages/api/v1/subscribe/[hash].api.ts
+++ pages/api/v1/subscribe/[hash].api.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { checkRequest } from 'pages/api/utils';
 import { supabase } from 'pages/api/utils/supabase';
-import { SUBSCRIBED_HASHES_TABLE } from 'pages/api/consts';
-import type { SubscribedHashTable, SubscribeResponse } from 'pages/api/@types';
+import { NOTIFICATIONS_TABLE, SUBSCRIBED_HASHES_TABLE } from 'pages/api/consts';
+import {
+  NotificationsTable,
+  SubscribedHashTable,
+  SubscribeResponse,
+} from 'pages/api/@types';
 
 export default async function handler(
   req: NextApiRequest,
@@ -24,13 +28,23 @@ export default async function handler(
     .from<SubscribedHashTable>(SUBSCRIBED_HASHES_TABLE)
     .select()
     .filter('user', 'eq', user)
-    .filter('hash', 'eq', hash);
-  if (existingHash && existingHash.length > 0) {
+    .filter('hash', 'eq', hash)
+    .single();
+  if (existingHash) {
     await supabase
+      .from<NotificationsTable>(NOTIFICATIONS_TABLE)
+      .delete()
+      .match({ hash: existingHash.id });
+    const deleteResponse = await supabase
       .from<SubscribedHashTable>(SUBSCRIBED_HASHES_TABLE)
       .delete()
       .match({ user, hash });
-    return res.status(200).json({ status: 'deleted' });
+    return res
+      .status(deleteResponse.status)
+      .json({
+        status:
+          deleteResponse.status === 200 ? 'deleted' : deleteResponse.statusText,
+      });
   }
 
   const { data, status } = await supabase
