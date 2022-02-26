d8312a7007021d8875d4439b9f9ab865ef97ffc6
Taha <taha.ojari@gmail.com>
2022-02-26 21:44:29 +0330

    feat: create search utils

---
+++ features/Search/utils/index.ts
@@ -0,0 +1,14 @@
+import { validate as validateBTCAddress } from 'bitcoin-address-validation';
+import type { SearchTypes } from '../@types';
+
+export const validateQuery = (query: string) => {
+  const isAddress = validateBTCAddress(query);
+  const isTransaction = !!query.match(/^[a-fA-F0-9]{64}$/);
+  const type: SearchTypes | undefined = isAddress
+    ? 'address'
+    : isTransaction
+    ? 'transaction'
+    : undefined;
+
+  return { valid: isAddress || isTransaction, type };
+};
